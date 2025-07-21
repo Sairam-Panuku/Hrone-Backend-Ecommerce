831d79b96c1c065e1484795366bc7d290a4501c0
Sairam-Panuku <sairampanuku123@gmail.com>
2025-07-21 18:12:20 +0530

    Removed 2.py to clean MongoDB credentials

--- 2.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://sairampanuku:Sairam%40123@ecommerce.nwuoyeh.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce&tls=true"
-
-client = MongoClient(uri)
-print(client.server_info())  # This will force a connection
